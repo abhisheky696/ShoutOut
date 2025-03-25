32a2b56314f4fcce65145ae6f43c067bf1fd0706
Abhishek Yadav <130171935+abhisheky696@users.noreply.github.com>
2025-03-25 14:27:15 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -16,6 +16,7 @@ app.use(
     cors({
         origin: [
             "https://shoutout-xa1s.onrender.com",
+            "https://shoutoutlouder.vercel.app",
             "http://localhost:5173",
             "http://localhost:5174",    
         ],
