9110363eb3932274686aa31c09e821b46caf2bd5
ay1878760 <ay1878760@gmail.com>
2025-06-18 12:43:35 +0530

    fix bookmark page ui

--- client/src/components/Bookmarks.jsx
+++ client/src/components/Bookmarks.jsx
@@ -10,7 +10,7 @@ const Bookmarks = () => {
     useGetBookmarkedTweets(loggedInUser?._id);
     const bookmarkedTweets = useSelector((state) => state.user.bookmarks);
     return (
-        <div className="w-[50%] min-h-screen bg-white dark:bg-gray-900 border-l border-r">
+        <div className="w-[50%] h-[100vh] bg-white dark:bg-gray-900 border-l border-r">
             <div className="flex items-center gap-4 py-3 px-4 border-b">
                 <Link to="/" className="cursor-pointer">
                     <ArrowLeft className="text-gray-700 dark:text-white" />
@@ -19,7 +19,7 @@ const Bookmarks = () => {
                     Bookmarks
                 </h2>
             </div>
-            <div className="w-full max-h-[80vh] overflow-auto px-4 pb-4 scrollbar-thin scrollbar-thumb-gray-400">
+            <div className="w-full max-h-[100vh] overflow-auto px-4 pb-4 scrollbar-thin scrollbar-thumb-gray-400">
                 {bookmarkedTweets?.length > 0 ? (
                     bookmarkedTweets.map((tweet) => (
                         <Tweetcard key={tweet?._id} tweet={tweet} />
